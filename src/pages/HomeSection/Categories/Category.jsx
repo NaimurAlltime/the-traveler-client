69bce9271f245574389ec7fca8bbf26cdaf131e8
Naimur Rahman <naimur2231@gmail.com>
2023-09-06 20:46:03 +0600

    category section added

--- src/pages/HomeSection/Categories/Category.jsx
+++ src/pages/HomeSection/Categories/Category.jsx
@@ -1,3 +1,4 @@
+import "./Category.css";
 const Category = ({ category }) => {
   const { category_name, image } = category;
   return (
@@ -12,9 +13,11 @@ const Category = ({ category }) => {
           />
         </div>
 
-        <div className="absolute bottom-0 inset-x-0 z-10">
-          <div className="flex flex-col h-full p-4 sm:p-6">
-            <p className="mt-2 text-center text-white/[.8]">{category_name}</p>
+        <div className="absolute bottom-0 bg-[#1c170d] rounded-b-md opacity-60 mt-12 inset-x-0 z-10">
+          <div className="flex flex-col h-full pt-1 pb-2">
+            <p className="mt-2 text-center text-white text-xl font-semibold">
+              {category_name}
+            </p>
           </div>
         </div>
       </a>
